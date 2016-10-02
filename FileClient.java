acbb4034e42a1bcde89972b9874a2195b2ae8a30
Kartik Mudgal <kartikmudgal07@gmail.com>
2016-10-02 14:43:15 +0530

    removed uneeded imports

--- FileClient.java
+++ FileClient.java
@@ -1,16 +1,12 @@
 package fileDownloadServer;
 
-import java.io.BufferedReader;
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 //import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
